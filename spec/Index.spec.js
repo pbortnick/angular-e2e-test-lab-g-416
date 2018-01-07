1812d87c52f90c3af154234efc8ab94d540f0885
pbortnick <pbortnick@gmail.com>
2018-01-07 15:16:04 -0500

    done

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -3,5 +3,13 @@ var path = require('path');
 describe('angularjs homepage todo list', function() {
 	it('should add a new contact', function() {
 		browser.get('http://localhost:8080');
+        element(by.css('[value="add"]')).click();
+
+        var contactList = element.all(by.repeater('contact in vm.contacts'));
+        expect(contactList.count()).toEqual(4);
+        expect(contactList.get(2).element(by.model('contact.name')).getAttribute('value')).toBe('Tim');
+				expect(contactList.get(2).element(by.model('contact.name')).getAttribute('value')).toBe('3934203242');
+    });
+});
 	});
 });
